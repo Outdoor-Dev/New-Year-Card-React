2f1f058e7f0d39eba3cfa5e8f9a5329f6031a136
MLopez8282 <saltairpix@gmail.com>
2021-01-06 01:35:42 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,23 +2,31 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import city from "../../img/city.png";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<video
+				className="video"
+				autoPlay
+				loop
+				type="video/mp4"
+				src="https://v.ftcdn.net/02/76/08/40/700_F_276084056_KLAfY8LhyPAaMa2cip4ljPfaKnr2bU1m_ST.mp4">
+				Your browser does not support the video tag{" "}
+			</video>
+			<img className="city" src={city} />
+			<h4 className="wordCarousel">
+				<span>Happy New </span>
+				<div>
+					<ul className="flip3">
+						<li>Year</li>
+						<li>Chances</li>
+						<li>Luck</li>
+					</ul>
+				</div>
+			</h4>
 		</div>
 	);
 }
